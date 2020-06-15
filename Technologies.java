28327a10e17ee3d898fd8a1f9f59db9b9e521515
Balaji Rajan <53979333+BalajiRajan21@users.noreply.github.com>
2020-06-15 11:56:52 +0530

    Delete Technologies.java

--- Technologies.java
+++
@@ -1,8 +0,0 @@
-public class Technologies{
-	public static void main(String [] args){
-		if(args.length==2)
-			System.out.println(args[0]+" Technologies "+args[1]);
-		else
-			System.out.println("Please enter Valid Name and Location");
-	}
-}
